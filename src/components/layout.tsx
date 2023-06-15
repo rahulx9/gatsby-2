fffa60f426037440314653c654460f76aa506448
Rahul Kumar Mohanta <74450641+rahulx9@users.noreply.github.com>
2023-06-15 16:57:31 +0530

    Added layout and styles

---
+++ src/components/layout.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "gatsby";
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+} from "./layout.module.css";
+
+type PropType = {
+  pageTitle?: String;
+  children: JSX.Element | JSX.Element[];
+};
+
+const Layout: React.FC<PropType> = ({ pageTitle, children }) => {
+  return (
+    <div className={container}>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
