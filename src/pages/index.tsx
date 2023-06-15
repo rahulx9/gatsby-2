f9f3c2bfad4307cbe292d34190d7681e3fae2791
Rahul Kumar Mohanta <74450641+rahulx9@users.noreply.github.com>
2023-06-15 17:20:20 +0530

    using image plugins

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import { PageProps } from "gatsby";
 import React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        src="../images/clifford.jpg"
+      />
     </Layout>
   );
 };
