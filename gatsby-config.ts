54a2c2de5df65ebf9fdda964eec2f9037c2e44fa
Rahul Kumar Mohanta <74450641+rahulx9@users.noreply.github.com>
2023-06-20 17:31:13 +0530

    Add Dynamic Images from Data

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -12,6 +12,7 @@ const config: GatsbyConfig = {
     `gatsby-plugin-mdx`,
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
       options: {
